90cd8917f3579e03d78b4519478c06daa52646ea
End313234 <end313234@protonmail.com>
2022-07-11 18:37:09 -0300

    Overwrite theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.tsx"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				red: {
+					500: "#EF233C",
+				},
+			},
+			fontFamily: {
+				sans: "Poppins, sans-serif",
+				heading: "Work Sans, sans-serif",
+			},
+		},
 	},
 	plugins: [],
 };
